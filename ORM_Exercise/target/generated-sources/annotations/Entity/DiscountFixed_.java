43d7aefaf13d8950518035dc096aa7fb55beb3c4
Christian <coej@Christians-MacBook-Pro.local>
2016-09-15 15:22:26 +0200

    SP3 opgave 3 basic lavet, mangler blot test

---
+++ ORM_Exercise/target/generated-sources/annotations/Entity/DiscountFixed_.java
@@ -0,0 +1,11 @@
+package Entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-15T15:06:14")
+@StaticMetamodel(DiscountFixed.class)
+public class DiscountFixed_ extends DiscountType_ {
+
+
+}
---
+++ ORM_Exercise/target/generated-sources/annotations/Entity/DiscountType_.java
@@ -0,0 +1,13 @@
+package Entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-09-15T15:06:14")
+@StaticMetamodel(DiscountType.class)
+public class DiscountType_ extends Customer_ {
+
+    public static volatile SingularAttribute<DiscountType, Double> discount;
+
+}
